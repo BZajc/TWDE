823a57d361eccd25bc160946c9e48d10e7fee6b1
BZajc <bbb.zaj@gmail.com>
2023-07-02 22:51:59 +0200

    some random fixes

--- dist/index.b47b69c1.js
+++
@@ -1,2 +0,0 @@
-const o=document.querySelector(".booking-btn"),e=document.querySelector(".booking");o.addEventListener("click",(function(){e.scrollIntoView({behavior:"smooth"})}));
-//# sourceMappingURL=index.b47b69c1.js.map
