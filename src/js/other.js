ee7007c57db372f4585f0391fd7f66eeff19ce2a
BZajc <bbb.zaj@gmail.com>
2023-07-14 07:53:52 +0200

    added nav

--- src/js/other.js
+++ src/js/other.js
@@ -3,8 +3,9 @@ function other() {
   const bookingSection = document.querySelector(".booking");
   const offersButton = document.querySelector(".offers-btn");
   const offersSection = document.querySelector(".offers");
-  const ratingStarsBox = document.querySelector(".offers__rating");
   const ratingStars = document.querySelectorAll(".offers__rating img");
+  const currencyBtn = document.querySelector(".currency-btn");
+  const nav = document.querySelector(".nav");
 
   // Smooth scrolling to booking section
   function scrollToBooking() {
@@ -21,40 +22,55 @@ function other() {
   // Animation for rating stars
   function jumpingStarsAnim() {
     const animationDuration = 500; // Animation last time
-  
+
     function startAnimation() {
       ratingStars.forEach((star, index) => {
         const delay = index * 200;
         setTimeout(() => {
           star.classList.add("jump");
-  
+
           setTimeout(() => {
             star.classList.remove("jump");
           }, animationDuration);
         }, delay);
       });
     }
-  
+
     function startInterval() {
       startAnimation(); // Running animation on start
-  
+
       setInterval(() => {
         startAnimation(); // Running animation every 5 seconds
       }, 5000);
     }
-  
+
     function startCycle() {
       startInterval();
-  
+
       setInterval(() => {
         startInterval(); // Running interval every 5 seconds
       }, 10000);
     }
-  
+
     startCycle(); // Running animation cycle on start
   }
-  
+
   jumpingStarsAnim();
+
+  // Display nav for mobile view
+  window.addEventListener("scroll", function () {
+
+    // Get bottom border of the element
+    const currencyBtnRect = currencyBtn.getBoundingClientRect();
+
+    if (currencyBtnRect.bottom < 0) {
+      nav.style.zIndex = "100";
+      nav.style.opacity = "1";
+    } else {
+      nav.style.zIndex = "-100";
+      nav.style.opacity = "0";
+    }
+  });
 }
 
 export default other;
--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,6 @@ import displayImagesSection from "./displayImages";
 import other from "./other";
 import tiltingAnim from "./tiltingAnim";
 import exchangeRate from "./exchangeRate";
-import renderOffers from "./renderOffers";
 
 // Pixabay API for display images section
 displayImagesSection();
@@ -12,9 +11,6 @@ other();
 
 // Running tilting animation
 tiltingAnim(".booking__option", 20);
-
-// Render cards in offers section
-renderOffers();
 
 // Currency Converter
 exchangeRate();
