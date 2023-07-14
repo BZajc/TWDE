ddfff33792c4787268b848a6fca8117546c3124c
BZajc <bbb.zaj@gmail.com>
2023-07-14 08:06:35 +0200

    default currency set to EUR

--- src/js/exchangeRate.js
+++ src/js/exchangeRate.js
@@ -6,10 +6,11 @@ function exchangeRate() {
   const actualCurrencyOption = document.querySelector(".actual-currency-option");
 
   const API_KEY = "c08dafddce738313737daf36";
+  const localStorageKey = "selectedCurrency";
 
   async function getCurrency() {
     try {
-      const selectedCurrency = currencySelect.value;
+      const selectedCurrency = currencySelect.value || localStorage.getItem(localStorageKey) || "EUR";
       const response = await axios.get(`https://v6.exchangerate-api.com/v6/${API_KEY}/latest/PLN`);
       const rates = response.data.conversion_rates;
 
@@ -20,7 +21,9 @@ function exchangeRate() {
         el.textContent = `${convertedAmount.toFixed(2)} ${selectedCurrency}`;
       });
 
-      actualCurrencyOption.textContent = `Actual Currency: ${currencySelect.value}`;
+      actualCurrencyOption.textContent = `Actual Currency: ${selectedCurrency}`;
+
+      localStorage.setItem(localStorageKey, selectedCurrency);
     } catch (err) {
       console.log("There was an error while converting currency:", err);
     }
